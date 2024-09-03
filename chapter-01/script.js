b01dd8a5ba1fa0be2ae638e8a638b59267dd998a
Nitin shukla <nitin.shukla12004@gmail.com>
2024-09-04 01:41:10 +0530

    learned about how to create a react app using parcel as a bundler

--- chapter-01/script.js
+++ chapter-01/script.js
@@ -1,3 +1,7 @@
-const element = React.createElement("h1", {id: "ele"}, "Hello World!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const element = React.createElement("h1", {id: "ele"}, "Hello World❤!");
 const root = ReactDOM.createRoot(document.querySelector(".root"));
 root.render(element);
