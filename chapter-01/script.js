c19abacd508d35a904936e3dce34fd865a627d2f
Nitin shukla <nitin.shukla12004@gmail.com>
2024-09-04 23:24:51 +0530

    started learning about JSX

--- chapter-01/script.js
+++ chapter-01/script.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const element = React.createElement("h1", {id: "ele"}, "Hello World❤!");
+// const element = React.createElement("h1", {id: "ele"}, "Hello World❤!"); // Here we have created elements using core react code
+// const root = ReactDOM.createRoot(document.querySelector(".root"));
+// root.render(element);
+
+
+const jsxheading = <h1 id="ele">Hello World😍!</h1>; // Here we have created react elements using JSX
+
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(element);
+
+root.render(jsxheading);
