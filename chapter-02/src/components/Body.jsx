1095b28dc428af8aa28ddffdc5af507f56c7fc64
Nitin shukla <nitin.shukla12004@gmail.com>
2024-09-08 06:05:33 +0530

    learned about shimmer UI and monolith vs microservices architecture

--- chapter-02/src/components/Body.jsx
+++ chapter-02/src/components/Body.jsx
@@ -1,23 +1,44 @@
 import CardCompoent from "./CardComponent";
-import restDetails from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useState, useEffect } from "react";
 
 const Body = () => {
+  let [restaurantList, setrestaurantList] = useState([]); // Local state variable
+  console.log(restaurantList);
 
-  let [restaurantList, setrestaurantList] = useState(restDetails); // Local state variable 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = async () => {
+    let data = await fetch(
+      "https://www.swiggy.com/mapi/homepage/getCards?lat=26.8756&lng=80.9115"
+    );
+
+    let jsonData = await data.json();
+    console.log(jsonData);
+    setrestaurantList(jsonData?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+  };
+
+  if(restaurantList.length === 0){
+    return (
+      <Shimmer />
+    )
+  }
   return (
     <div className="body">
       <div className="filter">
-        <button id="btn" onClick={() => {
-          let filteredList = restaurantList.filter((res) =>
-             res.info.externalRatings.aggregatedRating.rating > 4
-        );
-        setrestaurantList(filteredList);
-        setTimeout(() => {
-          setrestaurantList(restDetails)
-        }, 5000);
-        }}>Top rated Restaurants</button>
+        <button
+          id="btn"
+          onClick={() => {
+            let filteredList = restaurantList.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setrestaurantList(filteredList);
+          }}
+        >
+          Top rated Restaurants
+        </button>
       </div>
       <div className="res-container">
         {restaurantList.map((resturant, index) => (
--- chapter-02/src/components/CardComponent.jsx
+++ chapter-02/src/components/CardComponent.jsx
@@ -2,7 +2,7 @@ import {CDN_URL} from "../utils/constants"
 
 const CardCompoent = (props) => {
     const { resdata } = props;
-    const { name, cuisines } = resdata.info;
+    const { name, cuisines, avgRating, isOpen } = resdata.info;
     return (
       <div className="rest-card">
         <img
@@ -11,8 +11,8 @@ const CardCompoent = (props) => {
         ></img>
         <h4>{name}</h4>
         <h4>{cuisines.join(",")}</h4>
-        <h4>{resdata.info.externalRatings.aggregatedRating.rating} stars</h4>
-        <h4>{resdata.info.availability.opened ? "Open" : "Closed"}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{isOpen ? "Open" : "Closed"}</h4>
       </div>
     );
   };
