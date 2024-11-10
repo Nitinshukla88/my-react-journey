1ccffc2deec8c8d4e0a24842331431a5124ae4d4
nitinshukla <nitin.shukla12004@gmail.com>
2024-11-10 21:46:19 +0530

    written integraton test successfully for cart flow

--- chapter-02/src/components/__tests__/Cart.test.jsx
+++ chapter-02/src/components/__tests__/Cart.test.jsx
@@ -1,22 +1,60 @@
+
+// Best Integration test so far...
+
 import { act, fireEvent, render, screen } from "@testing-library/react";
 import RestaurantMenuCard from "../RestaurantMenuCard";
-import NEW_MOCK_DATA from "../mocks/ResMenuCardMock.json"; // This is default import 
+import NEW_MOCK_DATA from "../mocks/ResMenuCardMock.json"; // This is default import
 import "@testing-library/jest-dom";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
+import { Header } from "../Header";
+import { BrowserRouter } from "react-router-dom";
+import Cart from "../Cart";
 
-global.fetch = jest.fn(() => Promise.resolve({json: () => Promise.resolve(NEW_MOCK_DATA)}));
+global.fetch = jest.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve(NEW_MOCK_DATA) })
+);
 
 test("should verify the cart flow in our app", async () => {
+  await act(async () =>
+    render(
+        <BrowserRouter>
+        <Provider store={appStore}>
+            <Header/>
+            <RestaurantMenuCard />
+            <Cart/>
+        </Provider>
+        </BrowserRouter>
+    )
+  );
 
-    await act(async () => render(<Provider store={appStore}><RestaurantMenuCard/></Provider>));
+  const accordian = screen.getByText("McCafe. (23)");
 
-    const accordian = screen.getByText("Dessert (4)");
 
-    // expect(accordian).toBeInTheDocument();
+  expect(accordian).toBeInTheDocument();
 
-    fireEvent.click(accordian);
+  fireEvent.click(accordian);
 
-    expect(screen.getAllByTestId("food-items").length).toBe(4);
+  expect(screen.getAllByTestId("food-items").length).toBe(23);
+
+  const addBtns = screen.getAllByRole("button", {name : "Add+"});
+
+  fireEvent.click(addBtns[0]);
+
+  expect(screen.getByText("Cart - (1 items)")).toBeInTheDocument();
+
+  const cart = screen.getByText("Cart - (1 items)");
+
+  fireEvent.click(cart);
+
+  expect(screen.getAllByTestId("food-items").length).toBe(24);
+
+  fireEvent.click(screen.getByRole("button", {name : "Clear Cart"}));
+
+  expect(screen.getAllByTestId("food-items").length).toBe(23);
+
+  expect(screen.getByText("Cart - (0 items)")).toBeInTheDocument();
+
+  expect(screen.getByText("Cart is empty. Add more items")).toBeInTheDocument();
 
 });
